d535483ae763eb3392d3696545cbca0a70b24be1
Federico Luna <spikebh@gmail.com>
2022-05-26 21:23:26 -0300

    - charselect scene partially complete
    - Character select scene kinda functional.
    - Started working on the main scene, some tiles prefabs created.

---
+++ Assets/Scripts/CharSelectManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharSelectManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] characters;
+    [SerializeField] private int selectedCharacter;
+    // Start is called before the first frame update
+
+    public void NextCharacter()
+    {
+        characters[selectedCharacter].SetActive(false);
+        selectedCharacter = (selectedCharacter + 1) % characters.Length;
+        characters[selectedCharacter].SetActive(true);
+    }
+
+
+    public void PreviousCharacter()
+    {
+        characters[selectedCharacter].SetActive(false);
+        selectedCharacter--;
+        if (selectedCharacter < 0)
+        {
+            selectedCharacter += characters.Length;
+        }
+        characters[selectedCharacter].SetActive(true);
+
+    }
+
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(2, LoadSceneMode.Single);
+    }
+
+    private void Awake()
+    {
+        selectedCharacter = 0;
+        characters[selectedCharacter].SetActive(true);
+    }
+}
