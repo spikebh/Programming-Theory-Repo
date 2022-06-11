159308ed17011c9d18312c508b2196c81cb7619f
Federico Luna <spikebh@gmail.com>
2022-06-11 14:03:30 -0300

    - main game now loads the selected model
    - Main scene loads the selected model correctly.
    - Some work on player movement.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : Player
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rbPlayer = GameObject.FindWithTag("Player").GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
