b007d12bb6e02b3e435e8baec3e1875863d115b0
Federico Luna <spikebh@gmail.com>
2022-07-25 22:02:49 -0300

    Animations implemented
    - Idle, Run and Rotation implemented.
    - Minor level fixes

--- Assets/Scripts/DogMovement.cs
+++ Assets/Scripts/DogMovement.cs
@@ -7,11 +7,22 @@ public class DogMovement : MonoBehaviour
     [SerializeField] private float dogSpeed = 500.0f;
     [SerializeField] private float dogJumpSpeed = 50.0f;
     [SerializeField] public Rigidbody rbDog;
-
     [SerializeField] private bool isGrounded;
+    private Animator dogAnimation;
+
+    public bool rotateToLeft = false;
+    private float rotation = 180.0f;
+    private Vector3 rotationLeft;
+    private Vector3 rotationRight;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+        rotationLeft = new Vector3(x: 0f, y: rotation, z: 0f);
+        rotationRight = new Vector3(x: 0f, y: -rotation, z: 0f);
+        dogAnimation = GetComponent<Animator>();
         rbDog = GameObject.FindWithTag("Dog").GetComponent<Rigidbody>();
 
     }
@@ -26,11 +37,20 @@ public class DogMovement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space) && isGrounded) rbDog.AddForce(Vector3.up * dogJumpSpeed, ForceMode.Impulse);
 
-        if (Input.GetKey(KeyCode.D)) rbDog.AddForce(Vector3.right * dogSpeed);
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rbDog.AddForce(Vector3.right * dogSpeed, ForceMode.Force);
+            dogAnimation.SetBool("isMoving", true);
+            MovingRight();
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rbDog.AddForce(Vector3.left * dogSpeed, ForceMode.Force);
+            dogAnimation.SetBool("isMoving", true);
+            MovingLeft();
 
-        if (Input.GetKey(KeyCode.A)) rbDog.AddForce(Vector3.left * dogSpeed);
-
-
+        }
+        else dogAnimation.SetBool("isMoving", false);
     }
 
     void OnCollisionStay(Collision collision)
@@ -42,4 +62,30 @@ public class DogMovement : MonoBehaviour
     {
         isGrounded = false;
     }
+
+    void MovingRight()
+    {
+
+        //Debug.Log(rbDog.transform.rotation.y);
+        if (!rotateToLeft) return;
+        else
+        {
+            rbDog.transform.Rotate(rotationLeft);
+            rotateToLeft = false;
+        }
+    }
+
+    void MovingLeft()
+    {
+
+        //Debug.Log(rbDog.transform.rotation.y);
+        if (rotateToLeft) return;
+        else
+        {
+            rbDog.transform.Rotate(rotationRight);
+            rotateToLeft = true;
+        }
+
+    }
 }
+
--- Assets/Scripts/FoxMovement.cs
+++ Assets/Scripts/FoxMovement.cs
@@ -8,11 +8,20 @@ public class FoxMovement : MonoBehaviour
     [SerializeField] private float foxJumpSpeed = 50.0f;
     [SerializeField] public Rigidbody rbFox;
     [SerializeField] private bool isGrounded;
+    private Animator foxAnimator;
+
+    public bool rotateToLeft = false;
+    private float rotation = 180.0f;
+    private Vector3 rotationLeft;
+    private Vector3 rotationRight;
 
     // Start is called before the first frame update
     void Start()
     {
+        rotationLeft = new Vector3(x: 0f, y: rotation, z: 0f);
+        rotationRight = new Vector3(x: 0f, y: -rotation, z: 0f);
         rbFox = GameObject.FindWithTag("Fox").GetComponent<Rigidbody>();
+        foxAnimator = GetComponent<Animator>();
 
     }
 
@@ -26,11 +35,21 @@ public class FoxMovement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space) && isGrounded) rbFox.AddForce(Vector3.up * foxJumpSpeed, ForceMode.Impulse);
 
-        if (Input.GetKey(KeyCode.D)) rbFox.AddForce(Vector3.right * foxSpeed);
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rbFox.AddForce(Vector3.right * foxSpeed, ForceMode.Force);
+            foxAnimator.SetBool("isMoving", true);
+            MovingRight();
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rbFox.AddForce(Vector3.left * foxSpeed, ForceMode.Force);
+            foxAnimator.SetBool("isMoving", true);
+            MovingLeft();
+        }
+        else foxAnimator.SetBool("isMoving", false);
 
-        if (Input.GetKey(KeyCode.A)) rbFox.AddForce(Vector3.left * foxSpeed);
-
-
+        //MotionCheck();
     }
 
     void OnCollisionStay(Collision collision)
@@ -41,5 +60,30 @@ public class FoxMovement : MonoBehaviour
     void OnCollisionExit(Collision collision)
     {
         isGrounded = false;
+    }
+
+    void MovingRight()
+    {
+
+        //Debug.Log(rbFox.transform.rotation.y);
+        if (!rotateToLeft) return;
+        else
+        {
+            rbFox.transform.Rotate(rotationLeft);
+            rotateToLeft = false;
+        }
+    }
+
+    void MovingLeft()
+    {
+
+        //Debug.Log(rbFox.transform.rotation.y);
+        if (rotateToLeft) return;
+        else
+        {
+            rbFox.transform.Rotate(rotationRight);
+            rotateToLeft = true;
+        }
+
     }
 }
