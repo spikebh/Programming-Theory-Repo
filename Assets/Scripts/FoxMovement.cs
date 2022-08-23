fe8dc0545e3d8bdff566300a41dbb960ff6ea81d
Federico Luna <spikebh@gmail.com>
2022-08-23 18:31:05 -0300

    - Dash ability added.
    1. Dash ability added.
    2. Some minor code changes.

--- Assets/Scripts/FoxMovement.cs
+++ Assets/Scripts/FoxMovement.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FoxMovement : MonoBehaviour
+public class FoxMovement : Player // INHERITANCE
 {
-    [SerializeField] private float foxSpeed = 500.0f;
-    [SerializeField] private float foxJumpSpeed = 50.0f;
+    [SerializeField] private float playerSpeed = 2000.0f;
+    [SerializeField] private float playerJumpSpeed = 25.0f;
     [SerializeField] public Rigidbody rbFox;
     [SerializeField] private bool isGrounded;
     private Animator foxAnimator;
-
     public bool rotateToLeft = false;
     private float rotation = 180.0f;
     private Vector3 rotationLeft;
     private Vector3 rotationRight;
+    private float dashSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -22,34 +22,40 @@ public class FoxMovement : MonoBehaviour
         rotationRight = new Vector3(x: 0f, y: -rotation, z: 0f);
         rbFox = GameObject.FindWithTag("Fox").GetComponent<Rigidbody>();
         foxAnimator = GetComponent<Animator>();
+        dashSpeed = playerSpeed * 2f;
 
     }
 
     // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift)) PlayerDash();
+        else playerSpeed = 2000.0f;
+    }
+
     void FixedUpdate()
     {
-        if (!MainManager.Instance.gameOver) FoxMotion();
+        if (!MainManager.Instance.gameOver) FoxMotion(); // ABSTRACTION
     }
 
     public void FoxMotion()
     {
-        if (Input.GetKey(KeyCode.Space) && isGrounded) rbFox.AddForce(Vector3.up * foxJumpSpeed, ForceMode.Impulse);
+        if (Input.GetKey(KeyCode.Space) && isGrounded) rbFox.AddForce(Vector3.up * playerJumpSpeed, ForceMode.Impulse);
 
         else if (Input.GetKey(KeyCode.D))
         {
-            rbFox.AddForce(Vector3.right * foxSpeed, ForceMode.Force);
+            rbFox.AddForce(Vector3.right * playerSpeed, ForceMode.Force);
             foxAnimator.SetBool("isMoving", true);
-            MovingRight();
+            MovingRight(); // ABSTRACTION
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            rbFox.AddForce(Vector3.left * foxSpeed, ForceMode.Force);
+            rbFox.AddForce(Vector3.left * playerSpeed, ForceMode.Force);
             foxAnimator.SetBool("isMoving", true);
-            MovingLeft();
+            MovingLeft(); // ABSTRACTION
         }
         else foxAnimator.SetBool("isMoving", false);
 
-        //MotionCheck();
     }
 
     void OnCollisionStay(Collision collision)
@@ -85,5 +91,11 @@ public class FoxMovement : MonoBehaviour
             rotateToLeft = true;
         }
 
+    }
+
+    public override void PlayerDash()
+    {
+        base.PlayerDash(); // POLYMORPHISM
+        playerSpeed = dashSpeed;
     }
 }
